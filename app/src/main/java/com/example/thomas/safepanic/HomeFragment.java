5d9e866f23019fcc2c80ec1908d6f7dff02f149b
ThomasSoares <soaresthomas24@gmail.com>
2018-12-01 12:02:46 +0530

    Getting the current location

--- app/src/main/java/com/example/thomas/safepanic/HomeFragment.java
+++ app/src/main/java/com/example/thomas/safepanic/HomeFragment.java
@@ -1,23 +1,44 @@
 package com.example.thomas.safepanic;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class HomeFragment extends Fragment {
 
+    View parentHolder;
+    Button helpButton;
+
+
+    public void listeners()
+    {
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(parentHolder.getContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        parentHolder=inflater.inflate(R.layout.fragment_home, container, false);
+
+        helpButton=parentHolder.findViewById(R.id.helpButton);
+
+        listeners();
+
+        return parentHolder;
     }
 
 
--- app/src/main/java/com/example/thomas/safepanic/LoginActivity.java
+++ app/src/main/java/com/example/thomas/safepanic/LoginActivity.java
@@ -1,9 +1,19 @@
 package com.example.thomas.safepanic;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -24,6 +34,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     ConstraintLayout background;
     boolean rememberMeChecked;// needed to see if user checkmarked the remember me or not
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+    double longtitute;
+    double latitude;
+
     public void initialize()
     {
         //INITIALIZING ALL VARIABLES
@@ -38,6 +53,45 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         belowLoginText=findViewById(R.id.belowLoginText);
         background=findViewById(R.id.background);
         rememberMeChecked=false;
+
+        //ASK USER FOR PERMISSION
+        locationManager= (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener=new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("Location",location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+        else
+        {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0,locationListener);
+            Location lastKnowLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if(lastKnowLocation!=null)
+            {
+                updateLocationInfo(lastKnowLocation);
+            }
+        }
     }
 
     public void listeners()
@@ -51,6 +105,30 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         createTextView.setOnClickListener(this);
         belowLoginText.setOnClickListener(this);
         background.setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+        {
+            startListening();
+        }
+    }
+
+    public void startListening()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0,locationListener);
+        }
+    }
+
+    public void updateLocationInfo(Location location)
+    {
+        longtitute=location.getLongitude();
+        latitude=location.getLatitude();
     }
 
     @Override
@@ -98,6 +176,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
             if(emailEditText.getText().toString().equals("admin") && passwordEditText.getText().toString().equals("admin"))
             {
+                LocalStorage store=new LocalStorage(getApplicationContext());
+                store.addStorage("longtitude", String.valueOf(longtitute));
+                store.addStorage("latitude", String.valueOf(latitude));
                 Intent intent=new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
             }
