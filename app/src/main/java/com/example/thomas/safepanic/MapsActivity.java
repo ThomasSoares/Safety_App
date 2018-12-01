083af5f99dbc0432452e9f0a07b6accdd794fa30
ThomasSoares <soaresthomas24@gmail.com>
2018-12-01 18:32:05 +0530

    Added multiple dummy placeholders for display purposes

--- app/src/main/java/com/example/thomas/safepanic/MapsActivity.java
+++ app/src/main/java/com/example/thomas/safepanic/MapsActivity.java
@@ -1,12 +1,15 @@
 package com.example.thomas.safepanic;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -25,6 +28,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -37,5 +41,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng yourLocation = new LatLng(longtitude, latitude);
         mMap.addMarker(new MarkerOptions().position(yourLocation).title("Your location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(yourLocation, 14.2f));
+
+        LatLng help1 = new LatLng(18.5245031, 73.8247716);
+        mMap.addMarker(new MarkerOptions().position(help1).title("Ujwal Singhania").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+        LatLng help2 = new LatLng(18.5272873, 73.8424033);
+        mMap.addMarker(new MarkerOptions().position(help2).title("Suravarpu Gautam").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+        LatLng help3 = new LatLng(18.5287757, 73.8294494);
+        mMap.addMarker(new MarkerOptions().position(help3).title("Grishma Thapa").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        Location startPoint=new Location("locationA");
+        startPoint.setLatitude(latitude);
+        startPoint.setLongitude(longtitude);
+
+        Location endPoint=new Location("locationA");
+        endPoint.setLatitude(73.8294494);
+        endPoint.setLongitude(18.5287757);
+
+        double distance=startPoint.distanceTo(endPoint);
+        Toast.makeText(getApplicationContext(),String.valueOf(distance),Toast.LENGTH_LONG).show();
     }
 }
